0e3704da34f695b33e5c932ec131c9281f77e8d5
Misha-m-st <mikhailmakarov2005@gmail.com>
2025-02-25 18:26:33 +0300

    feat: Add multiplication function)

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 
 int addition(int a, int b);
 int subtraction(int a, int b);
-
+int multiply(int a, int b);
 
 int main() {
     int a = 4;
     int b = 9;
     std::cout << "Sum: " << addition(a, b) << std::endl;
     std::cout << "Subtraction: " << subtraction(a, b) << std::endl;
-
+    std::cout << "Multiply: " << multiply(a, b) << std::endl;
 
 
     return 0;
